7357eb56fe73072d846ff13d6e5b398b1fa0988f
Clifford18 <cliffordjohn.cj@gmail.com>
2022-05-04 17:02:00 +0300

    update App.js on routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Profile from "./Pages/Profile";
+import ErrorPage from "./Pages/ErrorPage";
 
 function App() {
 	return (
 		<Router>
 			<Routes>
-				<Route path="/"/>
+				<Route path="/" element={<Home/>} />
+				<Route path="/about" element={<About/>} />
+				<Route path="/profile" element={<Profile/>} />
+				<Route path="*" element={<ErrorPage/>} />
 
 			</Routes>
 		</Router>
