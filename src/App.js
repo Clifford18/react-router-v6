05268418d6f01647b43cf8431a8d84d63e7758dd
Clifford18 <cliffordjohn.cj@gmail.com>
2022-05-04 18:29:49 +0300

    update App.js file on profile def

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
 			<Routes>
 				<Route path="/" element={<Home/>} />
 				<Route path="/about" element={<About/>} />
-				<Route path="/profile" element={<Profile/>} />
+				<Route path="/profile/:username" element={<Profile/>} />
 				<Route path="*" element={<ErrorPage/>} />
 
 			</Routes>
