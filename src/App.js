e5b47bc8adfee48bc3e9bbd682f32a327e451085
Clifford18 <cliffordjohn.cj@gmail.com>
2022-05-04 17:18:26 +0300

    update App.js fileon links

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import {BrowserRouter as Router, Link, Route, Routes} from 'react-router-dom';
 import Home from "./Pages/Home";
 import About from "./Pages/About";
 import Profile from "./Pages/Profile";
@@ -8,6 +8,12 @@ import ErrorPage from "./Pages/ErrorPage";
 function App() {
 	return (
 		<Router>
+			<nav>
+				<Link to="/">Home</Link>
+				<Link to="/about" >About </Link>
+				<Link to="/profile" >Profile </Link>
+
+			</nav>
 			<Routes>
 				<Route path="/" element={<Home/>} />
 				<Route path="/about" element={<About/>} />
